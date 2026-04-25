68231d09c755d4a5e4a0877de90d1be024749f5d
faroukmbob <faroukmbob@gmail.com>
2026-04-25 16:54:21 +0300

    profile added

--- frontend/my-react-app/src/App.jsx
+++ frontend/my-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import ProfileSettings from "./profile";
 import "./App.css"; 
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
     <div className="app">
       {page === "signin" && <SignIn onNavigate={setPage} />}
       {page === "signup" && <SignUp onNavigate={setPage} />}
+      <ProfileSettings />
     </div>
   );
 }
